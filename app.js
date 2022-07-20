c0338089b106a21b71ec3b11328f4db50c4592ab
Paul Niemczyk <mrpndev@gmail.com>
2022-07-20 14:48:16 -0400

    Imported constants and dependencies; setup partial middlewares; setup our listener.

---
+++ app.js
@@ -0,0 +1,20 @@
+require("dotenv").config()
+
+const Express = require("express"),
+    cors = require("cors"),
+    mongoose = require("mongoose"),
+    app = Express(),
+    PORT = process.env.PORT || 4000,
+    HOST = process.env.HOST || "127.0.0.1"
+    db = require("./db")
+
+app.use(cors())
+app.use(Express.json())
+
+app.listen(PORT, HOST, () => {
+    try {
+        console.log(`server running on: ${HOST}:${PORT}`)
+    } catch(err) {
+        console.log(`server error: ${err}`)
+    }
+})
